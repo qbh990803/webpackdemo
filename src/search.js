2560eb22f490612392b297bca806fd77fc82c249
QiuBinHua <qiubh@tele-zone.cn>
2020-06-22 17:20:36 +0800

    常用loader和常用pligns

--- src/search.js
+++ src/search.js
@@ -1,1 +1,18 @@
-export default document.write('search page');
+import React from 'react';
+import ReactDom from 'react-dom';
+import "./search.less";
+import bird from  "./assets/images/brid.jpg";
+
+class Search extends React.Component {
+    render() {
+        return <div className="search-text">
+            请输入文字...
+            <img src={bird}></img>
+        </div>
+    }
+}
+
+ReactDom.render(
+    <Search />,
+    document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        index:'./src/index.js',
-        search: './src/search.js'
-    },
-    output: {
-        path: path.join(__dirname,'dist'),
-        filename: '[name].js',
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {test:'/\.css$/',use:'css-loader'},
-            { test: /\.ts$/, use: 'ts-loader'}
-        ]
-    }
-}
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.less$/, use: ["style-loader", "css-loader","less-loader"] },
+      { test: /\.(png|jpg|jpeg|gif)$/, use: ["file-loader"] },
+      { test: /\.js$/, use: "babel-loader" },
+      { test: /\.ts$/, use: "ts-loader" },
+    ],
+  },
+};
