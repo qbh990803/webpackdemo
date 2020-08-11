45e6f91472e489c56601a61b9279c7a3a660d2cd
QiuBinHua <qiubh@tele-zone.cn>
2020-08-11 11:34:20 +0800

    feat:
    文件指纹策略

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -7,16 +8,21 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name].js",
+    filename: "[name]_[chunkhash:8].js",
   },
   mode: "production",
   module: {
     rules: [
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.less$/, use: ["style-loader", "css-loader","less-loader"] },
-      { test: /\.(png|jpg|jpeg|gif)$/, use: ["file-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      { test: /\.less$/, use: [MiniCssExtractPlugin.loader, "css-loader","less-loader"] },
+      { test: /\.(png|jpg|jpeg|gif)$/, use: [{ loader:"file-loader",options: {name: '[name]_[hash:8].[ext]'} }],},
       { test: /\.js$/, use: "babel-loader" },
       { test: /\.ts$/, use: "ts-loader" },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    })
+  ]
 };
