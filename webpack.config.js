8a64f1d102de9885438387baa0a050461dd443b8
QiuBinHua <qiubh@tele-zone.cn>
2020-08-11 11:38:14 +0800

    Revert "feat:"
    This reverts commit 45e6f91472e489c56601a61b9279c7a3a660d2cd.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -8,21 +7,16 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name]_[chunkhash:8].js",
+    filename: "[name].js",
   },
   mode: "production",
   module: {
     rules: [
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
-      { test: /\.less$/, use: [MiniCssExtractPlugin.loader, "css-loader","less-loader"] },
-      { test: /\.(png|jpg|jpeg|gif)$/, use: [{ loader:"file-loader",options: {name: '[name]_[hash:8].[ext]'} }],},
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.less$/, use: ["style-loader", "css-loader","less-loader"] },
+      { test: /\.(png|jpg|jpeg|gif)$/, use: ["file-loader"] },
       { test: /\.js$/, use: "babel-loader" },
       { test: /\.ts$/, use: "ts-loader" },
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name]_[contenthash:8].css'
-    })
-  ]
 };
